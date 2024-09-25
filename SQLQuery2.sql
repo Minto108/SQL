5a261be82bd0053ad6b5f721530df8aca3b8f454
Minto108 <158478797+Minto108@users.noreply.github.com>
2024-09-25 22:03:46 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+--INSERT INTO EmployeeDemographics VALUES
+--(1001,'Jim','Harper',30,'M'),
+--(1002,'Minto','Kuruvilla',21,'M'),
+--(1003,'Sreyas','George',21,'M')
+
+--CREATE TABLE EmployeeSalary
+--(EmployeeID int,
+--JobTitle varchar(20),
+--Salary int)
+
+
+INSERT INTO EmployeeSalary VALUES
+(1001,'Salesman',45000),
+(1002, 'Data Analyst',60000),
+(1003, 'Software Engineer',65000)
---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+--SELECT * FROM EmployeeDemographics;
+
+--SELECT FirstName FROM EmployeeDemographics;
+
+--SELECT TOP 5 * FROM EmployeeDemographics;
+
+SELECT DISTINCT(EmployeeID) FROM EmployeeDemographics;
